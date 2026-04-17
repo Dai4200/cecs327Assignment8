8c791e307571355c60b29e8f9771f22a2c499410
Dai4200 <diego.ascencio01@student.csulb.edu>
2026-04-17 15:19:01 -0700

    Connected to db and queried count, going to run in server now

--- dbconnectiontest.py
+++ dbconnectiontest.py
@@ -1,6 +1,19 @@
+import psycopg2
 
-def main{
-  pass
-}
+def main():
+    conn = psycopg2.connect(
+        "postgresql://neondb_owner:npg_SkA08nzqVJaN@ep-crimson-rice-am6t1zgy.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    )
 
-main();
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) AS total_entries FROM "My_IoT_Table_virtual";')
+
+    result = cur.fetchone()  # returns a single row
+    total_entries = result[0]
+
+    print("Total entries:", total_entries)
+
+
+    cur.close()
+    conn.close()
+main()
